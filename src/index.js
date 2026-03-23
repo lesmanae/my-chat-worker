b1397e271ace11b7a4cb3df81d46a2aec3a7d011
lesmanae <fajarlsmn24@gmail.com>
2026-03-23 14:19:11 +0700

    add cors support

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ export default {
   async fetch(request) {
     const url = new URL(request.url);
 
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        status: 204,
+        headers: corsHeaders()
+      });
+    }
+
     if (request.method === "GET" && url.pathname === "/") {
       return json({
         ok: true,
@@ -52,7 +59,16 @@ function json(data, status = 200) {
   return new Response(JSON.stringify(data, null, 2), {
     status,
     headers: {
-      "content-type": "application/json; charset=utf-8"
+      "content-type": "application/json; charset=utf-8",
+      ...corsHeaders()
     }
   });
+}
+
+function corsHeaders() {
+  return {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type"
+  };
 }
